ca4cd599ddb5cc2d526a0d50b2a6a115fab7fc43
Latesh Bansal <78337739+lateshbansal@users.noreply.github.com>
2022-08-02 09:23:19 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,28 @@
+<?php
+
+    session_start();
+
+    $con = mysqli_connect('localhost','root','');
+
+    mysqli_select_db($con, 'easyrecharge');
+
+    $username = $_POST['username'];
+    
+    $password = $_POST['password'];
+
+    $_SESSION['username']=$username;
+
+    $q = " select * from registration where username = '$username' && password ='$password' ";
+
+    $result = mysqli_query($con,$q);
+
+    $row = mysqli_num_rows($result);
+
+    if($row == 1){
+        header('location:home.php');	
+    }
+    else{
+        header('location:index.html');
+    }
+
+?>
